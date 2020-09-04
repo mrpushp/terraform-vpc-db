f9f4b010280e57f0b8dba0063714f81f281268a5
hkantare@in.ibm.com <hkantare@in.ibm.com>
2020-09-04 16:12:46 +0530

    Add ansible tasks to install pip and request

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "bastion_public_ip" {
+    value = module.bastion.floating_ip_address
+}
+
+output "vm_private_ip" {
+    value = ibm_is_instance.frontend.primary_network_interface.0.primary_ipv4_address
+}
